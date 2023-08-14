9144fdb2b886ba9f062d6185a7151237c9827dd4
Edward Sung <edwardsung4217@gmail.com>
2023-08-14 10:55:07 -0400

    Template
    Simple template for loading instagram photos. Depending on data (photos) loaded the table is dynamic and will change to fit a Xx3 table/ Currently using api that gets beers

---
+++ script.js
@@ -0,0 +1,24 @@
+const apiUrl = 'https://random-data-api.com/api/v2/beers?size=5';
+
+// Function to fetch data from the API and populate the grid
+async function populateGrid() {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data)
+    const gridContainer = document.getElementById('gridContainer');
+
+    data.forEach(item => {
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('grid-item');
+
+    const name = document.createElement('p');
+    name.textContent = item.name;
+
+    gridItem.appendChild(name);
+
+    gridContainer.appendChild(gridItem);
+    });
+}
+
+// Call the function to populate the grid
+populateGrid();
